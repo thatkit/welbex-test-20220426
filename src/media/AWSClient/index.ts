5e94b95f33a41fdc06e9dab70d31869e12f7eccc
Nikita <emper137137@gmail.com>
2022-04-27 14:21:46 +1000

    Filebase S3 client integration setup

---
+++ src/media/AWSClient/index.ts
@@ -0,0 +1,64 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable prettier/prettier */
+require('dotenv').config();
+const AWS = require('aws-sdk');
+const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
+const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
+
+const s3Client = new AWS.S3({
+  endpoint: 'https://s3.filebase.com',
+  signatureVersion: 'v4',
+});
+
+// s3Client.listBuckets(function (err, data) {
+//     if (err) {
+//         console.log(err, err.stack);
+//     } else {
+//         const params = {
+//             Body: 'Hello, world!',
+//             Bucket: data['Buckets'][0]['Name'],
+//             Key: 'exampleobject',
+//             ContentType: 'text/plain',
+//             Metadata: {
+//                 metadata1: 'value1',
+//                 metadata2: 'value2',
+//             },
+//         };
+//         s3Client.putObject(params, function (err, data) {
+//             if (err) {
+//                 console.log(err, err.stack);
+//             } else {
+//                 console.log(data);
+//             }
+//         });
+//     }
+// });
+
+// // @ findAll objects
+// s3Client.listObjects({
+//     Bucket: 'welbex-test-bucket',
+// }, (err, data) => {
+//     if (err) return console.log(err);
+//     console.log(data)
+// });
+
+// @ findOne object
+s3Client.getObject({
+    Bucket: 'welbex-test-bucket',
+    Key: 'Фото 30на40(новый).png'
+}, (err, data) => {
+    if (err) return console.log(err);
+    console.log(data)
+});
+
+// @ get url for the media
+s3Client.getSignedUrlPromise('getObject', {
+    Bucket: 'welbex-test-bucket',
+    Key: 'ок ок.png'
+})
+    .then(url => console.log(url))
+    .catch(err => {
+        console.log('err:')
+        console.log(err)
+    })
+
