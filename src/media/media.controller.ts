77e8fa785ac664b9f63f78f4a35341dbb4cbef6b
Nikita <emper137137@gmail.com>
2022-04-30 14:13:31 +1000

    media service & controller adjustments

--- src/media/media.controller.ts
+++ src/media/media.controller.ts
@@ -1,26 +1,37 @@
 import {
   Controller,
+  Delete,
   Get,
+  Param,
   Post,
   UploadedFile,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 import { MediaService } from './media.service';
 
 @Controller('media')
 export class MediaController {
   constructor(private readonly mediaService: MediaService) {}
 
+  @UseGuards(JwtAuthGuard)
   @UseInterceptors(FileInterceptor('file'))
   @Post('file')
   create(@UploadedFile() file: Express.Multer.File) {
-    console.log('co:', file);
     return this.mediaService.createObject(file);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
-  findAllObjects() {
-    return this.mediaService.findAllObjects();
+  findAllObjects(blogNoteId: string) {
+    return this.mediaService.findAllObjects(blogNoteId);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete(':id')
+  delete(@Param() mediaRef: string) {
+    return 'hello deleted';
   }
 }
--- src/media/media.service.ts
+++ src/media/media.service.ts
@@ -1,31 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { FilebaseCustomClient } from './FilebaseCustomClient';
-// import { s3Client } from './AWSClient';
-import { CreateMediaDto } from './dto/create-media.dto';
-import { Media } from './entities/media.entity';
 
 @Injectable()
 export class MediaService {
-  constructor(
-    @InjectRepository(Media)
-    private mediaRepository: Repository<Media>,
-
-    private readonly fbClient: FilebaseCustomClient,
-  ) {}
-
-  async create(createMediaDto: CreateMediaDto) {
-    // # logic for downloading, uploading to AWS, and removing from backend
-    return this.mediaRepository.save(createMediaDto);
-  }
+  private readonly fbClient: FilebaseCustomClient;
 
   async createObject(file) {
-    console.log('se:', file);
     return this.fbClient.createObject(file);
   }
 
-  async findAllObjects() {
+  async findAllObjects(blogNoteId: string) {
     return await this.fbClient.findAllObjects();
+  }
+
+  async deleteObject(mediaRef: string) {
+    return 'hello im deleted';
   }
 }
