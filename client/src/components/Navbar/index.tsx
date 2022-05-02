4e87dd11284c9d9b871c40d3c96b527117e8d7f2
Nikita <emper137137@gmail.com>
2022-05-02 19:37:58 +1000

    navbar change

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -5,8 +5,7 @@ import styles from './styles.module.scss';
 export const Navbar = ({ data }: { data: any }) => { // # any
     return (
         <Nav className={styles.nav}>
-            <NavItem className={styles.topTitle} tag="h1">Blog</NavItem>
-            <NavItem className={styles.authorName} tag="h2">{data}</NavItem>
+            <NavItem className={styles.topTitle} tag="h1">{data}'s Blog</NavItem>
         </Nav>
     )
 }
