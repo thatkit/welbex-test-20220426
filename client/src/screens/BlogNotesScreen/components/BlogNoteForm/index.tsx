c46300e18eb6e36e132c202907c52315137f9deb
Nikita <emper137137@gmail.com>
2022-05-10 10:11:27 +1000

    auto insert value when focused

--- client/src/screens/BlogNotesScreen/components/BlogNoteForm/index.tsx
+++ client/src/screens/BlogNotesScreen/components/BlogNoteForm/index.tsx
@@ -5,37 +5,41 @@ import { useGlobalState } from '../../globalState';
 import { FilesInput } from '../FilesInput';
 import { v4 as uuid } from 'uuid';
 
-export const BlogNoteForm = observer(
-  ({ data }: { data: any }) => {
-    // # any
-    const [state] = useState(useGlobalState());
+export const BlogNoteForm = observer(({ data }: { data: any }) => {
+  // # any
+  const [state] = useState(useGlobalState());
 
-    return (
-      <Form>
-        <FormGroup floating>
-          <Input
-            id="title"
-            name="title"
-            placeholder={data.title || 'title'}
-            onChange={({ target }) => {
-              state.setTitleInput(target.value);
-              state.setIdInput(data.id || uuid());
-            }}
-          />
-          <Label for="title">Title</Label>
-        </FormGroup>
-        <FormGroup floating>
-          <Input
-            id="message"
-            name="message"
-            placeholder={data.message || 'message'}
-            type="textarea"
-            onChange={({ target }) => state.setMessageInput(target.value)}
-          />
-          <Label for="message">Message</Label>
-        </FormGroup>
-        <FilesInput data={data} />
-      </Form>
-    );
-  },
-);
+  return (
+    <Form>
+      <FormGroup floating>
+        <Input
+          id="title"
+          name="title"
+          placeholder={data.title || 'title'}
+          onFocus={({ target }) => {
+            if (data.title) target.value = data.title;
+          }}
+          onChange={({ target }) => {
+            state.setTitleInput(target.value);
+            state.setIdInput(data.id || uuid());
+          }}
+        />
+        <Label for="title">Title</Label>
+      </FormGroup>
+      <FormGroup floating>
+        <Input
+          id="message"
+          name="message"
+          placeholder={data.message || 'message'}
+          type="textarea"
+          onFocus={({ target }) => {
+            if (data.message) target.value = data.message;
+          }}
+          onChange={({ target }) => state.setMessageInput(target.value)}
+        />
+        <Label for="message">Message</Label>
+      </FormGroup>
+      <FilesInput data={data} />
+    </Form>
+  );
+});
